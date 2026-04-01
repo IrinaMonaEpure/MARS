733f94609fa4462ab05e47bcd060c55e7d86be0d
Irina Epure <irinamonaepure@gmail.com>
2026-04-01 11:51:13 +0200

    Config usage in embedding and connection

--- src/msean/generation/embed.py
+++ src/msean/generation/embed.py
@@ -1,17 +1,23 @@
 import numpy as np
 
+from msean.config import Config
+
 # Embedding nodes - steps 1 and 2a
 
-def distribute_nodes_uniformly(N:int, mu_min:float=0, mu_max:float=1, label_prefix:str='') -> dict:
+def distribute_nodes_uniformly(N:int, cfg:Config, label_prefix:str='') -> dict:
     """
     Distribute N nodes uniformly in the 2D plane mu_min <= x <= mu_max, mu_min <= y <=mu_max.
     
     Arguments:
         N (int): The number of nodes.
-        mu_min (float): The minimum value of x and y coordinates.
-        mu_max (float): The maximum value of x and y coordinates.
+        cfg (msean.Config): Model configuration.
 
     Returns:
         dict: Mapping of nodes 0 to N-1 to (x,y)-coordinates.
     """
-    return {f'{label_prefix}{i}': (np.random.uniform(mu_min, mu_max), np.random.uniform(mu_min, mu_max)) for i in range(N)}
+
+    mu_min = 0 # minimum value of x and y coordinates.
+    mu_max = mu_min + cfg.metric_space.square_side # maximum value of x and y coordinates
+
+    return {f'{label_prefix}{i}':
+            (np.random.uniform(mu_min, mu_max), np.random.uniform(mu_min, mu_max)) for i in range(N)}
